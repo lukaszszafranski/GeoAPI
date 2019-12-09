53ac7d4e1b7bcc7649f2c76db68f0fcd8a85f5e7
lukaszszafranski <lukasz.szafranski16@wp.pl>
2019-12-09 22:16:46 +0100

    Create PostData.cs

---
+++ GeolocationAPI/Models/PostData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeolocationAPI.Models
+{
+    public class PostData
+    {
+        public string IP { get; set; }
+    }
+}
